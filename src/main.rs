4d52e40dc56642526b8c8204f945b6561d08bc1b
murata0531 <coless115@gmail.com>
2021-11-01 04:52:13 +0900

    100スレッドで並列に書き込み

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use app::{
+    models::{self, NewUser, User},
+    schema,
+};
+use diesel::prelude::*;
+use rayon::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let concurrency = 100;
+    let n = 100000;
+
+    rayon::ThreadPoolBuilder::new().num_threads(concurrency as usize).build_global().unwrap();
+
+    let pool = models::create_db_pool(concurrency);
+
+    (1..=n).into_par_iter().for_each(|_|{
+        let conn = pool.get().unwrap();
+
+        let user = conn.transaction(|| {
+            diesel::insert_into(schema::users::table)
+                .values(&NewUser {
+                    name: "hoge".to_owned(),
+                })
+                .get_result::<User>(&conn)
+        });
+        // println!("{:?}", user);
+    });
 }
